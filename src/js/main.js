1f81a982804ac1d207c6f6cbba4e5bce006bafa0
sloppynick3 <sloppynick3@gmail.com>
2019-04-11 10:42:41 +0300

    made orders interactive

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,40 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function responsiveNavbar() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
+
+function changeOrder(order) {
+  //Some day improve this bad code
+  //Dont repeat yourself
+  switch (order) {
+    case 1:
+      document.getElementById('smallOrderSelector').className = "activeMobileOrderMenu";
+      document.getElementById('smallOrder').style.display = "inline-block";
+      document.getElementById('mediumOrderSelector').className = "";
+      document.getElementById('mediumOrder').style.display = "none";
+      document.getElementById('largeOrderSelector').className = "";
+      document.getElementById('largeOrder').style.display = "none";
+      break;
+    case 2:
+      document.getElementById('smallOrderSelector').className = "";
+      document.getElementById('smallOrder').style.display = "none";
+      document.getElementById('mediumOrderSelector').className = "activeMobileOrderMenu";
+      document.getElementById('mediumOrder').style.display = "inline-block";
+      document.getElementById('largeOrderSelector').className = "";
+      document.getElementById('largeOrder').style.display = "none";
+      break;
+    case 3:
+      document.getElementById('smallOrderSelector').className = "";
+      document.getElementById('smallOrder').style.display = "none";
+      document.getElementById('mediumOrderSelector').className = "";
+      document.getElementById('mediumOrder').style.display = "none";
+      document.getElementById('largeOrderSelector').className = "activeMobileOrderMenu";
+      document.getElementById('largeOrder').style.display = "inline-block";
+      break;
+  }
+}
