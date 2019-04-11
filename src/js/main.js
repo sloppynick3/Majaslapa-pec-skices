0435e06d6215bd697190981fd63717ed4cbc7f23
sloppynick3 <sloppynick3@gmail.com>
2019-04-11 21:46:15 +0300

    Added js to order page

--- src/js/main.js
+++ src/js/main.js
@@ -8,33 +8,76 @@ function responsiveNavbar() {
   }
 }
 
-function changeOrder(order) {
-  //Some day improve this bad code
-  //Dont repeat yourself
-  switch (order) {
+// For orders page
+var smallOrder = 0;
+var mediumOrder = 1;
+var largeOrder = 0;
+
+function setSmallOrderActive(status) {
+  console.log("Small order status" + status.toString());
+  if (1 == smallOrder) {
+    document.getElementById('smallOrderSelector').className = "activeMobileOrderMenu";
+    document.getElementById('smallOrder').style.display = "inline-block";
+  } else {
+    document.getElementById('smallOrderSelector').className = "";
+    document.getElementById('smallOrder').style.display = "none";
+  }
+}
+
+function setMediumOrderActive(status) {
+  if (1 == mediumOrder) {
+    document.getElementById('mediumOrderSelector').className = "activeMobileOrderMenu";
+    document.getElementById('mediumOrder').style.display = "inline-block";
+  } else {
+    document.getElementById('mediumOrderSelector').className = "";
+    document.getElementById('mediumOrder').style.display = "none";
+  }
+}
+
+function setLargeOrderActive(status) {
+  if (1 == largeOrder) {
+    document.getElementById('largeOrderSelector').className = "activeMobileOrderMenu";
+    document.getElementById('largeOrder').style.display = "inline-block";
+  } else {
+    document.getElementById('largeOrderSelector').className = "";
+    document.getElementById('largeOrder').style.display = "none";
+  }
+}
+
+function setOrders(x) {
+  switch (x) {
     case 1:
-      document.getElementById('smallOrderSelector').className = "activeMobileOrderMenu";
-      document.getElementById('smallOrder').style.display = "inline-block";
-      document.getElementById('mediumOrderSelector').className = "";
-      document.getElementById('mediumOrder').style.display = "none";
-      document.getElementById('largeOrderSelector').className = "";
-      document.getElementById('largeOrder').style.display = "none";
+      smallOrder = 1; mediumOrder = 0; largeOrder = 0;
       break;
     case 2:
-      document.getElementById('smallOrderSelector').className = "";
-      document.getElementById('smallOrder').style.display = "none";
-      document.getElementById('mediumOrderSelector').className = "activeMobileOrderMenu";
-      document.getElementById('mediumOrder').style.display = "inline-block";
-      document.getElementById('largeOrderSelector').className = "";
-      document.getElementById('largeOrder').style.display = "none";
+      smallOrder = 0; mediumOrder = 1; largeOrder = 0;
       break;
     case 3:
-      document.getElementById('smallOrderSelector').className = "";
-      document.getElementById('smallOrder').style.display = "none";
-      document.getElementById('mediumOrderSelector').className = "";
-      document.getElementById('mediumOrder').style.display = "none";
-      document.getElementById('largeOrderSelector').className = "activeMobileOrderMenu";
-      document.getElementById('largeOrder').style.display = "inline-block";
+      smallOrder = 0; mediumOrder = 0; largeOrder = 1;
       break;
+  }
+
+  changeOrder();
+}
+
+function changeOrder() {
+      setSmallOrderActive(smallOrder);
+      setMediumOrderActive(mediumOrder);
+      setLargeOrderActive(largeOrder);
+}
+
+function resetOrder() {
+  document.getElementById('smallOrder').style.display = "inline-block";
+  document.getElementById('mediumOrder').style.display = "inline-block";
+  document.getElementById('largeOrder').style.display = "inline-block";
+}
+
+function checkSize() {
+  console.log("Resize");
+  var width = document.body.clientWidth;
+  if (width > 850) {
+    resetOrder();
+  } else {
+    changeOrder();
   }
 }
